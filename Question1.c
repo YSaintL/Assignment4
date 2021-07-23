20dcb74a86068a67e185de10a4b7c64e34d93f54
Alex Do <alex26do@gmail.com>
2021-07-23 19:02:56 -0400

    Added read function

--- Question1.c
+++ Question1.c
@@ -11,11 +11,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
-//#include <sys/stat.h>
+#include <sys/stat.h>
 //#include <time.h>
 //#include <sys/types.h>
 //#include <stdbool.h>
 
+
+int numResources = 4;
+
+struct CustomerRequest {
+    int resources[4];
+};
 /* RQ - take a request
       - check if the request will result in a safe state
       - if safe = update the allocation of matrix to allocate resources
@@ -73,7 +79,128 @@ int initAvailable(int available[m]){
     return available[m];
 }
 
-int main(int argc, char *argv[]) {
+int readFile(char* fileName, struct CustomerRequest customerArr[]);
 
-    int 
+int main(int argc, char *argv[]) {
+    // Store ints from argv into an int array
+    int numResources[argc-1];
+    for(int i = 0; i < argc-1; i++) {
+        numResources[i] = atoi(argv[i+1]); 
+    }
+
+    struct CustomerRequest customerArr[5] = {};
+    char fileName[14] = "sample4_in.txt";
+    char* fileNamePtr = fileName;
+
+    readFile(fileNamePtr, customerArr);
+    
+
+    int hello = 5;
+}
+
+
+int readFile(char* fileName, struct CustomerRequest customerArr[])//use this method in a suitable way to read file
+{
+	FILE *in = fopen(fileName, "r");
+	if(!in)
+	{
+		printf("Child A: Error in opening input file...exiting with error code -1\n");
+		return -1;
+	}
+	printf("%s\n", fileName);
+
+	struct stat st;
+	fstat(fileno(in), &st);
+	char* fileContent = (char*)malloc(((int)st.st_size+1)* sizeof(char));
+	fileContent[0]='\0';	
+	while(!feof(in))
+	{
+		char line[100];
+		if(fgets(line,100,in)!=NULL)
+		{
+			strncat(fileContent,line,strlen(line));
+		}
+	}
+	fclose(in);
+
+	char* command = NULL;
+	int customerCount = 0;
+	char* fileCopy = (char*)malloc((strlen(fileContent)+1)*sizeof(char));
+	strcpy(fileCopy,fileContent);
+	command = strtok(fileCopy,"\r\n");
+	while(command!=NULL)
+	{
+		customerCount++;
+		command = strtok(NULL,"\r\n");
+	}
+	// *threads = (Thread*) malloc(sizeof(Thread)*customerCount);
+
+	char* lines[customerCount];
+	command = NULL;
+	int i=0;
+	command = strtok(fileContent,"\r\n");
+	while(command!=NULL)
+	{
+		lines[i] = malloc(sizeof(command)*sizeof(char));
+		strcpy(lines[i],command);
+		i++;
+		command = strtok(NULL,"\r\n");
+	}
+
+	for(int k=0; k<customerCount; k++)
+	{
+		char* token = NULL;
+		int j = 0;
+		// int a;
+		token =  strtok(lines[k],",");
+		//making array to stash tokens
+		char *arr[20] = {NULL};
+		while(token!=NULL)
+		{
+			arr[j++] = token;
+			token = strtok(NULL, ",");
+//this loop tokenizes each line of input file
+//write your code here to populate instances of Thread to build a collection
+		}
+
+		// For testing: printing the strings inside of array
+		// for(a = 0; a < j; a++)
+		// {
+			//Thread currentThread th;
+			
+			// printf("%s\n", arr[a]);
+			
+		// }
+		
+        struct CustomerRequest currentCustomerRequest;
+        // for (int i = 0; i < 4; i++) {
+        //     arr
+        // }
+        // strncpy(currentCustomerRequest.resources, arr, sizeof(int) * numResources);
+
+        // while (*arr != (void *)0) {
+        //     printf("%s", &arr);
+        //     arr++;
+        // }
+
+        for (int i = 0; i < 4; i++) {
+            // printf("Begin: ");
+            // printf("%s \n", arr[i]);
+            currentCustomerRequest.resources[i] = atoi(arr[i]);
+        }
+        printf("\n");
+
+		// memcpy(currentCustomerRequest.tid, arr[0], 8); // 8 is the size of the arr[0] (a char pointer)
+	
+       	// currentCustomerRequest.start_T = atoi(arr[1]);
+
+    	// currentCustomerRequest.lifetime = atoi(arr[2]);
+
+		// printf("%d\n", currentCustomerRequest.lifetime);
+
+        customerArr[k] = currentCustomerRequest;
+
+        // strcpy(customerArr, array1);
+	}
+	return customerCount;
 }
